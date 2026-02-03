2527b191544d6c05b964c8be306fbdcb40af8f57
freethedev <freethedev@proton.me>
2026-02-03 22:49:59 +0100

    Add basic commit with deps and logos

---
+++ config-overrides.js
@@ -0,0 +1,27 @@
+const webpack = require("webpack");
+
+module.exports = function override(config) {
+    config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+        buffer: require.resolve("buffer/"),
+        process: require.resolve("process/browser"),
+        vm: require.resolve("vm-browserify"),
+        
+    };
+
+    config.plugins = [
+        ...(config.plugins || []),
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ];
+
+    config.module.rules[0].resolve = {
+        fullySpecified: false,
+    };
+
+    return config;
+};
