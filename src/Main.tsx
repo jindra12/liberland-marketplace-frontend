60901176fb1f32be249491fb04f0415002425d55
freethedev <freethedev@proton.me>
2026-03-14 16:13:26 +0100

    Add correct app header, resolve document warning

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 import { ThirdwebProvider } from "thirdweb/react";
 
 import { AntProvider } from "./components/AntProvider";
+import { AppRouteTitle } from "./components/AppRouteTitle";
 import { EndpointContextProvider } from "./components/EndpointContext";
 import { RouteErrorFallback } from "./components/RouteErrorFallback";
 import { AuthContextProvider } from "./components/AuthContext";
@@ -61,51 +63,54 @@ const config = new QueryClient({
 
 const Main: React.FunctionComponent = () => (
     <QueryClientProvider client={config}>
-        <EndpointContextProvider>
-            <TronContext>
-                <SolanaContext>
-                    <ThirdwebProvider>
-                        <AuthContextProvider>
-                            <BrowserRouter>
-                                <CartMutationProvider>
-                                    <AntProvider>
-                                        <React.Suspense fallback={<AppBootSkeleton />}>
-                                            <AppLayout>
-                                                <Routes>
-                                                    <Route Component={suspense(Splash)} path="/" />
-                                                    <Route Component={suspense(Jobs)} path="/jobs" />
-                                                    <Route Component={suspense(Companies)} path="/companies" />
-                                                    <Route Component={suspense(Identities)} path="/tribes" />
-                                                    <Route Component={suspense(ProductsServices)} path="/products-services" />
-                                                    <Route Component={suspense(Syndication)} path="/syndication" />
-                                                    <Route Component={suspense(Job)} path="/jobs/:id" />
-                                                    <Route Component={suspense(Company)} path="/companies/:id" />
-                                                    <Route Component={suspense(Identity)} path="/tribes/:id" />
-                                                    <Route Component={suspense(ProductService)} path="/products-services/:id" />
-                                                    <Route Component={suspense(SyndicationDetail)} path="/syndication/:id" />
-                                                    <Route Component={suspense(Profile)} path="/profile" />
-                                                    <Route Component={suspense(Publish)} path="/publish" />
-                                                    <Route Component={suspense(EditJob)} path="/jobs/edit/:id" />
-                                                    <Route Component={suspense(EditCompany)} path="/companies/edit/:id" />
-                                                    <Route Component={suspense(EditProduct)} path="/products-services/edit/:id" />
-                                                    <Route Component={suspense(Cart)} path="/cart" />
-                                                    <Route Component={suspense(Order)} path="/order" />
-                                                    <Route Component={suspense(Startups)} path="/ventures" />
-                                                    <Route Component={suspense(Startup)} path="/ventures/:id" />
-                                                    <Route Component={suspense(EditStartup)} path="/ventures/edit/:id" />
-                                                    <Route Component={suspense(AuthCallback)} path="/auth/callback" />
-                                                    <Route Component={suspense(NotFound)} path="*" />
-                                                </Routes>
-                                            </AppLayout>
-                                        </React.Suspense>
-                                    </AntProvider>
-                                </CartMutationProvider>
-                            </BrowserRouter>
-                        </AuthContextProvider>
-                    </ThirdwebProvider>
-                </SolanaContext>
-            </TronContext>
-        </EndpointContextProvider>
+        <HelmetProvider>
+            <EndpointContextProvider>
+                <TronContext>
+                    <SolanaContext>
+                        <ThirdwebProvider>
+                            <AuthContextProvider>
+                                <BrowserRouter>
+                                    <AppRouteTitle />
+                                    <CartMutationProvider>
+                                        <AntProvider>
+                                            <React.Suspense fallback={<AppBootSkeleton />}>
+                                                <AppLayout>
+                                                    <Routes>
+                                                        <Route Component={suspense(Splash)} path="/" />
+                                                        <Route Component={suspense(Jobs)} path="/jobs" />
+                                                        <Route Component={suspense(Companies)} path="/companies" />
+                                                        <Route Component={suspense(Identities)} path="/tribes" />
+                                                        <Route Component={suspense(ProductsServices)} path="/products-services" />
+                                                        <Route Component={suspense(Syndication)} path="/syndication" />
+                                                        <Route Component={suspense(Job)} path="/jobs/:id" />
+                                                        <Route Component={suspense(Company)} path="/companies/:id" />
+                                                        <Route Component={suspense(Identity)} path="/tribes/:id" />
+                                                        <Route Component={suspense(ProductService)} path="/products-services/:id" />
+                                                        <Route Component={suspense(SyndicationDetail)} path="/syndication/:id" />
+                                                        <Route Component={suspense(Profile)} path="/profile" />
+                                                        <Route Component={suspense(Publish)} path="/publish" />
+                                                        <Route Component={suspense(EditJob)} path="/jobs/edit/:id" />
+                                                        <Route Component={suspense(EditCompany)} path="/companies/edit/:id" />
+                                                        <Route Component={suspense(EditProduct)} path="/products-services/edit/:id" />
+                                                        <Route Component={suspense(Cart)} path="/cart" />
+                                                        <Route Component={suspense(Order)} path="/order" />
+                                                        <Route Component={suspense(Startups)} path="/ventures" />
+                                                        <Route Component={suspense(Startup)} path="/ventures/:id" />
+                                                        <Route Component={suspense(EditStartup)} path="/ventures/edit/:id" />
+                                                        <Route Component={suspense(AuthCallback)} path="/auth/callback" />
+                                                        <Route Component={suspense(NotFound)} path="*" />
+                                                    </Routes>
+                                                </AppLayout>
+                                            </React.Suspense>
+                                        </AntProvider>
+                                    </CartMutationProvider>
+                                </BrowserRouter>
+                            </AuthContextProvider>
+                        </ThirdwebProvider>
+                    </SolanaContext>
+                </TronContext>
+            </EndpointContextProvider>
+        </HelmetProvider>
     </QueryClientProvider>
 );
 
