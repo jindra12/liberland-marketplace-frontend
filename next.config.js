a091d19a48190d766ac39bcc446e91f6f80e5cee
jindra12 <jindra12.underdark@gmail.com>
2026-08-16 00:33:03 +0200

    Fix routing in pages, update vercel

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ loadEnvConfig(process.cwd());
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: Object.fromEntries(Object.entries(process.env).filter(([key]) => key.startsWith("REACT_APP_"))),
+    async rewrites() {
+        return [{
+            source: "/.well-known/oauth-protected-resource",
+            destination: "/api/oauth-protected-resource",
+        }];
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.md$/i,
